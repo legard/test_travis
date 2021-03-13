e427233bfdcc13c9db5f808ebeb1501384ae398f
Ruslan Tabolin <ruslan.tabolin@gmail.com>
2021-03-13 12:58:04 +0300

    Initial commit

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,9 @@
 # content of test_sample.py
+import pytest
+
 def inc(x):
     return x + 1
 
-
-def test_answer():
-    assert inc(3) == 4
+@pytest.mark.parametrize("expected_result", [3, 4, 5])
+def test_answer(expected_result):
+    assert inc(3) == expected_result
